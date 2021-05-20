948ec587c3a99d260a8a0e40b6c4a2c4bfb459f2
Joaquim Griñó Lladós <44608258+Vhisarion@users.noreply.github.com>
2021-05-20 14:42:53 +0200

    Initial Commit
    Commiting for the project. Essentially what you get on forge's MDK and a few new packages.

---
+++ src/main/java/vhisarion/vhisarionspracticestuff/VhisarionsPracticeStuff.java
@@ -0,0 +1,8 @@
+package vhisarion.vhisarionspracticestuff;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(value = "vhisarionspracticestuff")
+public class VhisarionsPracticeStuff {
+	public static final String MOD_ID = "vhisarionspracticestuff";
+}
